628bbda4ad599072282e4d8796c0dfa4a8547b6a
Yaser-123 <1ammar.yaser@gmail.com>
2025-12-11 00:27:33 +0530

    used proper lenis smooth function

--- ui/app/(landing)/page.tsx
+++ ui/app/(landing)/page.tsx
@@ -28,7 +28,7 @@ export default function LandingPage() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smooth: true,
+      smoothWheel: true,
     })
 
     function raf(time: number) {
